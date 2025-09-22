b26b48315cd383a5824d80316a90a048a1c0ed6a
S S Mohanty <ssmohanty@evawinoptimize.com>
2025-09-22 10:31:15 +0530

    intial commit to server

---
+++ APILogs/src/LogAllAPICalls.java
@@ -0,0 +1,6 @@
+
+public class LogAllAPICalls 
+{
+	DevTools devtools;
+
+}
---
+++ MavenJenkins/src/test/java/Test/Login.java
@@ -0,0 +1,32 @@
+package Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+
+
+public class Login 
+{
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void acc() throws InterruptedException, IOException 
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://uat.evaerp.cloud/");
+		
+		
+				
+	}
+}
